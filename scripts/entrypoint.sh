4af99df3c37de1c49e4108c68a9d1e17bb145d55
Ellie Johnston <jellie7118@proton.me>
2026-06-11 14:39:43 +0000

    YESSS CRON, BABY'S FIRST TASK HOLY SHIT

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -66,6 +66,6 @@ log "Starting cron ..."
 service cron start
 crontab /etc/cron.d/moodle
 
-chown www-data:www-data /var/www/html/moodle
+chown -R www-data:www-data /var/www/html/moodle
 log "Starting Apache ..."
 exec apache2ctl -D FOREGROUND
