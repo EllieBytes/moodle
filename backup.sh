ecc9042b7fbf7a29eb5682e327b2215b115b2e33
Ellie Johnston <jellie7118@proton.me>
2026-06-16 15:41:07 +0000

    Fix

--- backup.sh
+++ backup.sh
@@ -7,7 +7,7 @@ for VOL in $VOLUMES; do
     echo "Backing up $VOL"
 
     docker run --rm -v "${VOL}:/source:ro" -v "${BACKUPS}:/backup:ro" alpine \
-        tar czf "/backup/${VOL}-$(date +%Y_%m_%D_%H_%M).tgz" -C /source .
+        tar czf "/backup/${VOL}-$(date +%Y_%m_%d_%H_%M).tgz" -C /source .
 done
 
 echo "All backups complete."
